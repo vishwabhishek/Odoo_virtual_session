579529a5e9f8b52df5ef6ec7dda1c62d15dbf8e1
vishwabhishek <158059379+vishwabhishek@users.noreply.github.com>
2025-07-12 17:11:12 +0530

    Delete backend directory

--- backend/routes/admin.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {adminLogin, banUser} from "../controller/admin.controller.js";
-import auth from "../middleware/auth.js";
-const router = Router();
-
-router.get("/login",adminLogin);
-router.patch("/:id/ban", auth,banUser);
-
-export default router;
